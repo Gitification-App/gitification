72e7246ce59fa96cdd3c85f205cce96d53ab98f3
kadiryazici <kyzc411@outlook.com.tr>
2023-02-03 21:47:49 +0300

    Add checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
+import { checker as Checker } from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Vue(),
+    Checker({
+      vueTsc: true,
+    }),
     Icons({
       compiler: 'vue3',
       defaultClass: 'icon',
