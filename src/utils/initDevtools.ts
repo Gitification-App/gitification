5b12162f84e6aa53b7cb195e5e4e7a7e74a0980a
kadiryazici <kyzc411@outlook.com.tr>
2023-02-09 14:36:55 +0300

    Remove redundant dev mode check

--- src/utils/initDevtools.ts
+++ src/utils/initDevtools.ts
@@ -2,8 +2,6 @@
  * Only for DEV
  */
 export async function initDevtools() {
-  if (import.meta.env.DEV) {
-    const { connect } = await import('@vue/devtools')
-    connect('http://localhost', 8098)
-  }
+  const { connect } = await import('@vue/devtools')
+  connect('http://localhost', 8098)
 }
