1fb4ce753ba72f5f8ab792a9fe85e550310b789a
kadiryazici <kyzc411@outlook.com.tr>
2023-02-09 05:55:32 +0300

    Add upload release asset

--- actions/release.ts
+++ actions/release.ts
@@ -1,6 +1,10 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'node:fs/promises'
 import * as github from '@actions/github'
 
 const token = process.env.GITHUB_TOKEN
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 async function run() {
   const octokit = github.getOctokit(token)
@@ -12,6 +16,15 @@ async function run() {
     draft: true,
     name: 'test-version',
     body: 'This is just for testing',
+  })
+
+  octokit.rest.repos.uploadReleaseAsset({
+    owner: 'kadiryazici',
+    repo: 'gitification',
+    name: 'package.json',
+    release_id: release.data.id,
+    // @ts-expect-error type
+    data: (await fs.readFile(path.join(dirname, 'package.json'))).buffer,
   })
 }
 
