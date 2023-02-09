0e41637cdf2a53d371160439d1b97e00143b7eb7
kadiryazici <kyzc411@outlook.com.tr>
2023-02-09 15:30:22 +0300

    Update workflow with univesal build & version

--- actions/release.ts
+++ actions/release.ts
@@ -2,30 +2,56 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import fs from 'node:fs/promises'
 import child_process from 'child_process'
+import { promisify } from 'util'
 import * as github from '@actions/github'
+
+const exec = promisify(child_process.exec)
 
 const token = process.env.GITHUB_TOKEN!
 const dirname = path.dirname(fileURLToPath(import.meta.url))
-const dmgPath = path.join(dirname, '..', 'src-tauri', 'target', 'release', 'bundle', 'macos', 'Gitification_0.0.0_x64.dmg')
 
 async function run() {
+  await exec('rustup target add x86_64-apple-darwin')
+  await exec('rustup target add aarch64-apple-darwin')
+  await exec('pnpm tauri build --target universal-apple-darwin')
+
+  const tauriConf = JSON.parse(
+    await fs.readFile(path.join(dirname, '..', 'src-tauri', 'tauri.conf.json'), 'utf-8'),
+  ) as typeof import('../src-tauri/tauri.conf.json')
+
+  const packageJSON = JSON.parse(
+    await fs.readFile(path.join(dirname, '..', 'package.json'), 'utf-8'),
+  ) as typeof import('../package.json')
+
+  if (packageJSON.version !== tauriConf.package.version)
+    throw new Error('Tauri config and Package JSON versions are not the same, update both of them!')
+
+  const dmgFileName = `Gitification_${packageJSON.version}_universal.dmg`
+
+  const dmgPath = path.join(
+    dirname,
+    '..',
+    'src-tauri',
+    'target',
+    'universal-apple-darwin',
+    'release',
+    'bundle',
+    'dmg',
+    dmgFileName,
+  )
+
   const octokit = github.getOctokit(token)
-
-  child_process.execSync('pnpm tauri build', { stdio: 'inherit' })
-
   const release = await octokit.rest.repos.createRelease({
     owner: 'kadiryazici',
     repo: 'gitification',
-    tag_name: 'test-version',
-    draft: true,
-    name: 'test-version',
-    body: 'This is just for testing',
+    tag_name: packageJSON.version.toString(),
+    name: packageJSON.version.toString(),
   })
 
   octokit.rest.repos.uploadReleaseAsset({
     owner: 'kadiryazici',
     repo: 'gitification',
-    name: 'Gitification.aarch64.dmg',
+    name: dmgFileName,
     release_id: release.data.id,
     // @ts-expect-error type
     data: (await fs.readFile(dmgPath)).buffer,
