9588bc7bc8948a8c810552ad330e4352d37a1157
kadiryazici <kyzc411@outlook.com.tr>
2023-06-03 06:16:57 +0300

    Convert settings button to menu button

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: '@antfu',
   rules: {
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', fixStyle: 'inline-type-imports' }],
     'no-console': 'off',
     'vue/attribute-hyphenation': 'off',
     'vue/v-on-event-hyphenation': 'off',
