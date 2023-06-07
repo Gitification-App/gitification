f2a71d790b936279e17c07020ab1afc150e589bf
kadiryazici <kyzc411@outlook.com.tr>
2023-06-07 22:22:05 +0300

    Add release workflow

--- .github/actions/constants.ts
+++
@@ -1,5 +0,0 @@
-export const OWNER = 'Gitification-App'
-export const REPO = 'gitification'
-export const token = process.env.GITHUB_TOKEN
-export const secret = process.env.CLIENT_SECRET
-export const id = process.env.CLIENT_ID
