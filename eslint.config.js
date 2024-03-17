57a6706aebacb35b30bb18938201a902e6a7bfb5
kadiryazici <kyzc411@gmail.com>
2024-03-17 19:45:34 +0300

    Use based person's config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,3 @@
-import antfu from '@antfu/eslint-config'
+import config from '@kadiryazici/eslint-config'
 
-export default antfu({
-  rules: {
-    'no-console': 'off',
-    'vue/attribute-hyphenation': 'off',
-    'vue/v-on-event-hyphenation': 'off',
-    'ts/consistent-type-definitions': ['error', 'type'],
-    'vue/custom-event-name-casing': 'off',
-    'vue/max-attributes-per-line': ['error', {
-      singleline: {
-        max: 1,
-      },
-      multiline: {
-        max: 1,
-      },
-    }],
-    // Require curly always for if statements
-    'curly': ['error', 'all'],
-    // if else statements should be separated by a newline
-    'brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
-  },
-})
+export default config()
